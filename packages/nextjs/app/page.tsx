fec82083f1d1a299d30ccedd23b677bf7cf7e042
nodestarQ <node@perfectqstudios.com>
2024-07-13 16:58:08 +0200

    random commitment + notes creation

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,13 +1,50 @@
 "use client";
 
 import Link from "next/link";
+import { useState } from "react";
 import type { NextPage } from "next";
 import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { Address } from "~~/components/scaffold-eth";
+import { randomBytes } from 'crypto';
+import { keccak256 } from 'viem';
+import { ethers } from "ethers";
+
+
+function generateRandomByte32(): Uint8Array {
+  return randomBytes(32);
+}
+
 
 const Home: NextPage = () => {
   const { address: connectedAddress } = useAccount();
+  const [note, setNote] = useState("");
+
+  async function generateTwoSecrets(){
+
+    // let nullifierPreimage = [214,255,124,83,114,187,12,181,14,111,72,234,5,249,135,238,141,197,183,240,45,224,153,161,158,69,230,216,57,122,75,68];
+    // let secret = [230,146,124,238,124,120,160,205,44,185,238,166,139,63,96,30,207,97,7,85,117,207,245,240,45,97,6,100,229,191,245,27];
+    const abiEncoder = new ethers.utils.AbiCoder
+    const secret = generateRandomByte32();
+    const nullifierPreimage = generateRandomByte32();
+
+    //use this for Smart Contract interaction
+    const commitment = ethers.utils.keccak256(abiEncoder.encode(["bytes32","bytes32"], [nullifierPreimage, secret]))
+    const nullifierHash = ethers.utils.keccak256(nullifierPreimage);
+  
+    setNote('{"secret": "'+ethers.utils.hexlify(secret)+'",\n'+'"nullifierPreimage": "'+ethers.utils.hexlify(nullifierPreimage)+'"\n}');
+  }
+  
+  async function downloadNote() {
+    await generateTwoSecrets()
+    const element = document.createElement("a");
+    const file = new Blob([note], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = "note.json";
+    document.body.appendChild(element); // Required for this to work in FireFox
+    element.click();
+    document.body.removeChild(element);
+  }
 
   return (
     <>
@@ -41,26 +78,7 @@ const Home: NextPage = () => {
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <BugAntIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Tinker with your smart contract using the{" "}
-                <Link href="/debug" passHref className="link">
-                  Debug Contracts
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
-            <div className="flex flex-col bg-base-100 px-10 py-10 text-center items-center max-w-xs rounded-3xl">
-              <MagnifyingGlassIcon className="h-8 w-8 fill-secondary" />
-              <p>
-                Explore your local transactions with the{" "}
-                <Link href="/blockexplorer" passHref className="link">
-                  Block Explorer
-                </Link>{" "}
-                tab.
-              </p>
-            </div>
+            <button className="btn" onClick={async()=>{await downloadNote()}}>randomBuffer</button>
           </div>
         </div>
       </div>
